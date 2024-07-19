bf3708808866fbbf110c877e6cfec2e992ca8b4e
BenoistP <bp_github.com@gazouille.net>
2024-07-19 13:56:52 +0200

    Update mtplx.storage.imports.ts

--- app/imports/mtplx.storage.imports.ts
+++ app/imports/mtplx.storage.imports.ts
@@ -14,3 +14,21 @@
 
 // Create a generic file directly.
 // createGenericFile('some content', 'my-file.txt', { contentType: "text/plain" });
+
+
+import {
+  createBrowserFileFromGenericFile as MPL_F_createBrowserFileFromGenericFile,
+  createGenericFile as MPL_F_createGenericFile,
+  createGenericFileFromBrowserFile as MPL_F_createGenericFileFromBrowserFile,
+  createGenericFileFromJson as MPL_F_createGenericFileFromJson,
+  parseJsonFromGenericFile as MPL_F_parseJsonFromGenericFile,
+} from '@metaplex-foundation/umi';
+
+
+export {
+  MPL_F_createBrowserFileFromGenericFile,
+  MPL_F_createGenericFile,
+  MPL_F_createGenericFileFromBrowserFile,
+  MPL_F_createGenericFileFromJson,
+  MPL_F_parseJsonFromGenericFile,
+}
