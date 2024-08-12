7f3050ee37bfbeeda8e2889b15f152237b7f0722
BenoistP <75934369+BenoistP@users.noreply.github.com>
2024-08-12 20:27:45 +0200

    first test

--- tests/poap-alyra.ts
+++ tests/poap-alyra.ts
@@ -2,6 +2,11 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PoapAlyra } from "../target/types/poap_alyra";
 
+// Seeds
+const SeedUserDataAccount = "SoaplanaUserData";
+const SeedUserMintsAccount = "SoaplanaUserMints";
+const SeedUserBurnsAccount = "SoaplanaUserBurns";
+
 describe("poap-alyra", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -10,7 +15,35 @@ describe("poap-alyra", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    // First "mint"
+    const firstRandomNftMintPubkey = anchor.web3.PublicKey.unique();
+    const firstRandomNftMints = [firstRandomNftMintPubkey];
+
+    const [pdaUserData, ] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(SeedUserDataAccount), program.provider.publicKey.toBuffer()],
+      program.programId,
+    )
+    const [pdaUserMints, ] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(SeedUserMintsAccount), program.provider.publicKey.toBuffer()],
+      program.programId,
+    )
+    const [pdaUserBurns, ] = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from(SeedUserBurnsAccount), program.provider.publicKey.toBuffer()],
+      program.programId,
+    )
+    const tx = await program.methods.initialize( firstRandomNftMints ).accounts({
+      userData: pdaUserData,
+      userMints: pdaUserMints,
+      userBurns: pdaUserBurns,
+  }).rpc();
     console.log("Your transaction signature", tx);
   });
 });
+
+
+const getRandomPositiveInt = (max: number = 1) => {
+  const defaultVal = 1;
+  const randomNum = max < 0 ? defaultVal : Math.floor(Math.random() * max) + 1;
+  console.info(`randomNum = ${randomNum}`);
+  return randomNum;
+};
